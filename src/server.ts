a2228b2a6f4a89e2bd07f1a5580c8ef1ba35b1af
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-30 11:05:20 +0600

    basic express setup

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { json, Request, Response } from "express";
+const app = express();
+const port = 5000;
+
+// parser
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Next Level Developer!");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
